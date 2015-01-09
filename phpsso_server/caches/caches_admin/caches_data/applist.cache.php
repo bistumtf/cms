1a33c10ab93fc9bf4344bdf85a64f7efd8111272
bistumtf <bistumtf@163.com>
2015-01-09 17:39:53 +0800

    f

---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://phpcms/',
+    'authkey' => 'khYUVDwnqGQ9rg99n05yxmKOmz4N6Nwi',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
